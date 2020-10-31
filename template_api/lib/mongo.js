740d4ca63386abdb2871cd1aaafb68aec5be1832
backDAJG <jerez1.2.3.102107@gmail.com>
2020-10-31 18:38:53 -0500

    create interaction database

--- template_api/lib/mongo.js
+++ template_api/lib/mongo.js
@@ -9,7 +9,7 @@ const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.d
 
 class MongoLib {
   constructor() {
-    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true  });
     this.dbName = DB_NAME;
   }
 
@@ -20,15 +20,52 @@ class MongoLib {
           if (err) {
             reject(err);
           }
-          console.log('Connected succesfully to mongo')
-          resolve(this.client.db(this.dbName))
+          console.log('Connected succesfully to mongo');
+          resolve(this.client.db(this.dbName));
         });
       });
     }
 
-    return MongoLib.connection
+    return MongoLib.connection;
+  }
+
+  getAll(collection, query) {
+    return this.connect().then((db) => {
+      return db.collection(collection).find(query).toArray();
+    });
+  }
+
+  get(collection, id) {
+    return this.connect().then((db) => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
+
+  create(collection, data) {
+    return this.connect()
+      .then((db) => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then((result) => result.insertedId);
+  }
+
+  update(collection, id, data) {
+    return this.connect()
+      .then((db) => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then((result) => result.upsertedId || id);
+  }
+
+  delete(collection, id) {
+    return this.connect()
+      .then((db) => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
   }
 }
 
-
-module.exports = MongoLib
+module.exports = MongoLib;
