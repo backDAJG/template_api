de107b938f7b046337018aebf27fe027aea28810
backDAJG <jerez1.2.3.102107@gmail.com>
2020-11-01 23:10:08 -0500

    authentication google OAuth 2.0

---
+++ ssr-server/index.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const passport = require("passport");
+const boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
+
+const { config } = require("./config/index");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+//Basic strategy
+require("./utils/auth/strategies/basic");
+//OAuth Strategy
+require("./utils/auth/strategies/oauth");
+// Agregamos las variables de timpo en segundos
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
+
+app.post("/auth/sign-in", async function (req, res, next) {
+  passport.authenticate("basic", function (error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      const { token, user } = data;
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error);
+        }
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("auth/sign-up", async (req, res, next) => {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user,
+    });
+    res.status(201).json({ message: "user created" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/movies", async (req, res, next) => {});
+
+app.post("/user-movies", async function (req, res, next) {
+  try {
+    const { body: userMovie } = req;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "post",
+      data: userMovie,
+    });
+
+    if (status !== 201) {
+      console.log("bad");
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/user-movies/:userMovieId", async function (req, res, next) {
+  try {
+    const { userMovieId } = req.params;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "delete",
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openid"],
+  })
+);
+
+app.get(
+  "auth/google-oauth/callback",
+  passport.authenticate("google-oauth", { session: false }),
+  (req, res, next) => {
+    if(!req.user) {
+      next(boom.unauthorized())
+    }
+
+    const { token, ...user } = req.user
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    })
+    res.status(200).json(user)
+  }
+);
+
+app.listen(config.port, () => {
+  console.log(`listening http://localhost:${config.port}`);
+});
+
+/*
+OAuth 2.0 es un estándar de la industria que nos permite implementar autorización.
+
+OpenID Connect es una capa de autenticación que funciona sobre OAuth con métodos ya construidos.
+*/
